61ff50421d0d11d9697e72b1cc4c3103efc33959
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-15 17:22:06 +0100

    Added costructor to SillyView

--- PlayerManagerMVC/SillyView.cs
+++ PlayerManagerMVC/SillyView.cs
@@ -7,6 +7,12 @@ namespace PlayerManagerMVC
 {
     public class SillyView : IView
     {
-        
+        private readonly Controller controller;
+        private readonly List<Player> playerList;
+        public SillyView(Controller controller, List<Player> playerList)
+        {
+            this.controller = controller;
+            this.playerList = playerList;
+        }
     }
 }
